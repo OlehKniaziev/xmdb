08957e827fac4f9e669001b8accdc58a9c6842d8
OlehKniaziev <atmatm9182@gmail.com>
2026-01-06 19:09:32 +0100

    Better `warn` logging function scoping inside `XMDB_FIXME`

--- src/Core/Logger.hpp
+++ src/Core/Logger.hpp
@@ -41,6 +41,6 @@ void error(const char *fmt, ...) OK_ATTRIBUTE_PRINTF(1, 2);
 
 #define XMDB_FIXME(msg)                                                                                                \
     do {                                                                                                               \
-        log::warn("%s:%d: [%s] FIXME: %s", __FILE__, __LINE__, __FUNCTION__, (msg));                                        \
+        ::xmdb::log::warn("%s:%d: [%s] FIXME: %s", __FILE__, __LINE__, __FUNCTION__, (msg));                           \
     } while (false)
 } // namespace xmdb
