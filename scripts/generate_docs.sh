ac2763e8fc2abbeaa9b58e3c48ea37009f025d77
OlehKniaziev <atmatm9182@gmail.com>
2026-03-19 11:55:14 +0100

    Added a github workflow for docs, modified the `generate_docs.sh` script

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
-set -xe
+set -e
 
-doxygen
+if test "$@" -lt "1"; then
+    echo "Not enough arguments"
+    exit 1
+fi
 
-XMDB_ROOT=$(pwd) sphinx-build -M html ./sphinx/source ./docs
+if [ "$1" == "html" ]; then
+    doxygen
+
+    XMDB_ROOT=$(pwd) sphinx-build -M html ./sphinx/source ./docs
+elif [ "$1" == "pdf" ]; then
+    doxygen
+
+    XMDB_ROOT=$(pwd) sphinx-build -M latex ./sphinx/source ./latex-docs
+    pushd ./latex-docs/latex
+    make
+    cp ./xmdb.pdf ../../
+    popd
+
+    echo "Generated xmdb.pdf in the current directory"
+else
+    echo "Uncrecognized target format '$1'"
+    exit 1
+fi
