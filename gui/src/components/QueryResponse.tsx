bae4b8ea4082706d24a3d7c92311ab67a195c929
lizakitte <lizakitte@gmail.com>
2026-03-20 11:45:58 +0100

    added image visualization

--- gui/src/components/QueryResponse.tsx
+++ gui/src/components/QueryResponse.tsx
@@ -1,11 +1,30 @@
-import type { QueryResponse } from "../data/query-response";
+import type { ImageChunk, QueryResponse as QueryResponseType } from "../data/query-response";
 import LoadingSpinner from "./LoadingSpinner";
+import { useMemo } from "react";
+import { imageHexToDataUrl } from "../data/util";
+
+function ImageDisplay({ chunk }: { chunk: ImageChunk }) {
+  const src = useMemo(
+    () => imageHexToDataUrl(chunk.data, chunk.width, chunk.height),
+    [chunk.data, chunk.width, chunk.height]
+  );
+
+  return (
+    <img
+      src={src}
+      alt="PNG data"
+      width={chunk.width}
+      height={chunk.height}
+      style={{ maxWidth: "200px", maxHeight: "200px" }}
+    />
+  );
+}
 
 function QueryResponse({
   response,
   isLoading,
 }: {
-  response: QueryResponse;
+  response: QueryResponseType | undefined;
   isLoading: boolean;
 }) {
   if (isLoading) {
@@ -31,9 +50,25 @@ function QueryResponse({
               {response.rows.map((row) => {
                 return (
                   <tr>
-                    {response.column_names.map((columnName) => (
-                      <td>{row[columnName]}</td>
-                    ))}
+                    {response.column_names?.map((columnName, columnIndex) => {
+                      if (typeof row[columnName] === "object") {
+                        if (response.column_types?.[columnIndex] === "PNG") {
+                          const chunk = row[columnName] as ImageChunk;
+
+                          return (
+                            <td>
+                              <ImageDisplay chunk={chunk} />
+                            </td>
+                          );
+                        } else {
+                          return <td>{JSON.stringify(row[columnName])}</td>;
+                        }
+                      } else {
+                        return (
+                          <td>{row[columnName] as string | number | boolean}</td>
+                        );
+                      }
+                    })}
                   </tr>
                 );
               })}
